416564bd390ca36c507dd9695ff502d6796fa9ac
Tanuja Gupta <tanuja.gupta@msds.christuniversity.in>
2023-12-06 22:45:00 +0530

    upload all

---
+++ xyz.js
@@ -0,0 +1,21 @@
+setTimeout(function(){
+    var typed = new Typed('.element', {
+        strings: ["Tanuja Gupta"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        loop: true,
+        loopCount: Infinity,
+        startDelay: 1000
+    });
+    },1000)
+
+    setTimeout(function(){
+        var typed = new Typed('.element2', {
+            strings: ["Fresh Web Developer"],
+            typeSpeed: 100,
+            backSpeed: 100,
+            loop: true,
+            loopCount: Infinity,
+            startDelay: 1000
+        });
+        },1000)
